f461e859648ee700b1e60f0782cc78e12eb3d5af
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2022-08-31 09:12:56 -0400

    Project Files for Reason one with the "Corrected" code
    bruh why, we haven't even gotten to the if statement yet

---
+++ Combo/Combo/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub MathBtn_Click(sender As Object, e As EventArgs) Handles MathBtn.Click
+        ' Variables, this still hurts
+        Dim nmbr1 As Integer
+        Dim nmbr2 As Integer
+        Dim answer As Double ' yes, I know it's unused be quiet
+        nmbr1 = Number1.Text ' painfully setting variables, why do you teach like this.
+        nmbr2 = Number2.Text
+
+        ' Now I have to wait for everyone to do the variables, then do a dumb way of making an if statement. Smh
+
+
+    End Sub
+End Class
