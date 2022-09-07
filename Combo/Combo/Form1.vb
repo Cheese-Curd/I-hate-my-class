b94f12d62dd3f58f75763710a8337aa0240f76ce
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2022-09-07 06:43:02 -0700

    Made ints into "singles

--- Combo/Combo/Form1.vb
+++ Combo/Combo/Form1.vb
@@ -3,8 +3,8 @@
         ' 9/7/22 - "Learned how to do comments and if statements" bruh
 
         ' Variables, this still hurts
-        Dim nmbr1 As Integer
-        Dim nmbr2 As Integer
+        Dim nmbr1 As Single
+        Dim nmbr2 As Single
         Dim answer As Double ' this is an unneeded variable! why do you teach like this!
         nmbr1 = Number1.Text ' painfully setting variables, why do you teach like this.
         nmbr2 = Number2.Text
@@ -21,6 +21,6 @@
             answer = nmbr1 / nmbr2
         End If
 
-        OutputLabel.Text = answer '
+        OutputLabel.Text = answer
     End Sub
 End Class
