c74857299fa74ee96a2cf1624ec1120a887932c1
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2023-02-10 09:42:02 -0500

    Added some teacher examples with my comments

---
+++ TeacherExamples.py
@@ -0,0 +1,21 @@
+# all comments are added by me
+
+# Pulling things from Lists
+CS_Students = [name1, name2...] # she writes these really fast and didn't want to waste time ig
+name = input("Please enter your name.")
+
+if name in CS_Students:
+  class = input("Please enter your class") # Can't use class as a variable
+  if class = "Freshman":
+    print("you need to take Assesmt") # Missspelling
+  else:
+    print("Enjoy your CS class")
+
+else:
+  print("you should consider CS")
+
+# Random number with While Loops
+i = 3
+while i > 0:
+  print("you have " ; "guesses") # I get it's supposed to be quick, but uh, what is the semicolon doing? (or what I can assume is a semicolon, I don't know what that is lol)
+  guess = int(input("...")) # She was writing it quickly, with no room on the board, so this makes sense to not have anything in the input.
