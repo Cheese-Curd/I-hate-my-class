3bf9b504e53fd50eaff8942a64c2742cef7fad98
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2023-02-10 09:47:45 -0500

    dice

---
+++ dice.py
@@ -0,0 +1,5 @@
+# For context, she said once we were done with the Number Guess thing. We had to make a program to get 2 dice rolls and print them
+# This is what I did, literally easiest thing I've ever done
+
+import random
+print("You rolled a", random.randint(1, 6), "and a", random.randint(1, 6)) # literally no logic, just calling a function twice then printing it
